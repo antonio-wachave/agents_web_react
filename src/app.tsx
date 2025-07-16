7b3e6f8a1ba92bfcec73660ca444f3b01787428b
antonio-wachave <anthonypaulino32@gmail.com>
2025-07-16 17:04:02 +0200

    web commit

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query";   
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
+export function App() {
+  return (
+
+    <QueryClientProvider client={new QueryClient()}>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<CreateRoom/>} index/>
+        <Route element={<Room/>} path="/room/:roomId" />
+      </Routes>
+    </BrowserRouter>
+    </QueryClientProvider>  
+  );
+}
