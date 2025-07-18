53ee475ad4a2b4c359b92e5c59c2211af40d8bbd
antonio-wachave <anthonypaulino32@gmail.com>
2025-07-18 15:35:36 +0200

    badge,button,card,form,input,label,textarea

---
+++ src/components/create-room-form.tsx
@@ -0,0 +1,98 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import { z } from 'zod/v4';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+
+const createRoomSchema = z.object({
+  name: z.string().min(1, 'O nome da sala é obrigatório'),
+  description: z.string().optional(),
+});
+
+type CreateRoomFormData = z.infer<typeof createRoomSchema>;
+
+export function CreateRoomForm() {
+  const createRoomForm = useForm<CreateRoomFormData>({
+    resolver: zodResolver(createRoomSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+    },
+  });
+
+  function handleCreateRoom(data: CreateRoomFormData) {
+    console.log(data);
+    // Aqui você pode fazer a chamada para criar a sala, por exemplo, enviando os dados para uma API
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Criar Sala</CardTitle>
+        <CardDescription>
+          Criei uma nova sala para começar a fazer perguntas e receber respostas
+          da IA.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...createRoomForm}>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={createRoomForm.handleSubmit(handleCreateRoom)}
+          >
+            <FormField
+              control={createRoomForm.control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Nome da Sala</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Digite o nome da sala" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+
+            <FormField
+              control={createRoomForm.control}
+              name="description"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Descricao</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Digite a Descricao da sala" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+
+            <Button className="w-full" type="submit">
+              Criar Sala
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/http/use-rooms.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import type { GetRoomsAPIResponse } from './types/get-room-response';
+
+export function UseRooms() {
+  return useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms');
+      const result: GetRoomsAPIResponse = await response.json();
+
+      return result;
+    },
+  });
+}
